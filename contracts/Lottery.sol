a4e078ae75fb56625cfc299bf55fec154b367c60
parkjunsoo <01junsoo@gmail.com>
2022-04-28 17:25:26 +0900

    feat: 배팅 인스턴스 생성

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,7 +2,22 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Lottery {
+    struct BetInfo {
+        uint256 answerBlockNumber;
+        address payable bettor;
+        bytes1 challengers;
+    }
+    uint256 private _tail;
+    uint256 private _head;
+    mapping (uint256 => BetInfo) private _bets;
+
     address public owner;
+    uint256 constant internal BLOCK_LIMIT = 256;
+    uint256 constant internal BET_BLOCK_INTERVAL = 3;
+    uint256 constant internal BET_AMOUNT = 5 * 10 ** 15;
+
+    uint256 private _pot;
+
     constructor() {
         //배포가 될때 보낸사람을 owner로 저장한다.
         owner = msg.sender;
@@ -11,4 +26,34 @@ contract Lottery {
     function getSomeValue() public pure returns (uint256 value) {
         return 5;
     }
+
+    function getPot() public view returns (uint256 pot) {
+        return _pot;
+    }
+
+
+    function getBetInfo(uint256 index) public view returns (uint256 answerBlockNumber, address bettor, bytes1 challengers){
+        BetInfo memory b = _bets[index];
+        answerBlockNumber = b.answerBlockNumber;
+        bettor = b.bettor;
+        challengers = b.challengers;
+    }
+
+    function pushBet( bytes1 challengers) public returns (bool) {
+        BetInfo memory b;
+        b.bettor = payable(msg.sender);
+        b.answerBlockNumber = block.number + BET_BLOCK_INTERVAL;
+        b.challengers = challengers;
+
+        _bets[_tail] = b;
+        _tail++;
+        return true;
+    }
+
+    function popBet(uint256 index) public returns (bool){
+        delete _bets[index];
+        return true;
+    }
+
+
 }
--- test/lottery.test.js
+++ test/lottery.test.js
@@ -1,16 +1,24 @@
 const Lottery = artifacts.require("Lottery");
 
 contract("Lottery", function ([deployer, user1, user2]) {
+  let lottery;
   beforeEach(async () => {
     console.log("before, each");
-    Lottery = await Lottery.new();
+    lottery = await Lottery.new();
   });
-  it("Basic_test", async () => {
+
+  it("Basic test", async () => {
     console.log("Basic test");
-    const owner = await Lottery.owner();
-    const value = await Lottery.getSomeValue();
+    const owner = await lottery.owner();
+    const value = await lottery.getSomeValue();
     console.log(owner);
     console.log(value);
-    assert.equal(10, 10);
+    assert.equal(value, 10);
+  });
+
+  it.only("getPot should return current pot", async () => {
+    const pot = await lottery.getPot();
+    console.log(pot);
+    assert.equal(pot, 0);
   });
 });
